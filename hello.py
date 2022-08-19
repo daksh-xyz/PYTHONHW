d10ceda393aa8bea111d4a7328eacf03ecf886b4
daksh-xyz <dakshuklal@gmail.com>
2022-08-20 01:27:01 +0530

    Homework_ig?

---
+++ hello.py
@@ -0,0 +1,15 @@
+name = 'Daksh'
+age = '18'
+email = 'dakshuklal+vs@gmail.com'
+mobile = '8287086661'
+school = 'BBPSGRH'
+exe = (name,age,email,school,mobile)
+print(exe)
+
+                                                    #OR
+name = 'Daksh'
+age = '18'
+email = 'dakshuklal+vs@gmail.com'
+mobile = '8287086661'
+school = 'BBPSGRH'
+print(name,age,email,mobile)
