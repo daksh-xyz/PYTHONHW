31b067a5a523a87070de440e67733c415932201e
daksh-xyz <dakshuklal@gmail.com>
2022-08-21 01:12:39 +0530

    homeworkv2

--- Homework.py
+++ Homework.py
@@ -1,3 +1,6 @@
+from array import array
+
+
 intNum = 73
 floatNum = 73.73
 complexNum = 73j
@@ -37,3 +40,31 @@ Address = {Address}
 College = {College}
 '''
 print(result)
+
+#20-Aug-22
+name = 'Daksh'
+Name = 'daksh lal'
+print('capitalize:', name.capitalize())
+print('upper_case_eve:', name.upper())
+print('lower_case_eve:', name.lower())
+print('count:', name.count('a'))
+print('find_letter:',name.find("k"))
+print('tells if string ends with input:', name.endswith("l"))
+print('index/position:', name.index("a"))   #prints the position of the letter inside "" when it was used for the first time
+print(name.isalnum())
+print(name.isalpha())
+print(name.isdecimal())
+print(name.isdigit())   #true if all are digits
+print(name.islower())   #true if all chars are lowercase
+print(name.isprintable())
+print(name.replace("Daksh","Jatin")) #replaces specified value with another value given after a comma, add an int without'' to specify how many times to replace the word
+print(name.rsplit("a")) #splits at a given letter(deletes it too?), annd returns w a list
+print(Name.swapcase()) #swaps upper and lower cases duh
+print(Name.title())   #first char of each word in caps
+print(name.zfill(7))  #adds 0s to the beginning of the string until its length is as specified in the brackets
+print('\nLength:', len(name),'\n')  #prints the length of the variable in the bracket no '' reqd
+print('\n 1st char of variable(name): ', name[0], '\n')  #prints the first letter of the given word, requires [] brackets
+print('\n 0 to 4th char of variable(name): ', Name[0:5], '\n') #x:y, : adds range to print function to print from x till y-1, starts from 0
+print('\n 0 to 4th char of variable (name):', name[:5], '\n')  #if we dont put anything in 'x' it meas from start and if 'y' is empty it means for the length of the string  
+#NEGATIVE SLICING
+print('\n negative slicing:', Name[-3:-1], '\n')
