5c20c17b20d80f816396557291e078295279e99a
daksh-xyz <dakshuklal@gmail.com>
2022-08-26 15:52:11 +0530

    final

--- Homework_26.py
+++ Homework_26.py
@@ -1,20 +1,20 @@
-# user_input = input('Enter a value: ')
-# print('length of given input: ', user_input.__len__())
-# if user_input.strip().isdigit() == True:          #strip method removes a char from starting and ending, in our case it is removing '' which makes input a string 
-#     print('input number is an integer')   #is.digit tells if it is an integer in bool
-#     exit()                    # we use exit() because if it has identified the type of input we don't need it to identify it further/to stop code there
+user_input = input('Enter a value: ')
+print('length of given input: ', user_input.__len__())
+if user_input.strip().isdigit() == True:          #strip method removes a char from starting and ending, in our case it is removing '' which makes input a string 
+    print('input number is an integer')   #is.digit tells if it is an integer in bool
+    exit()                    # we use exit() because if it has identified the type of input we don't need it to identify it further/to stop code there
 
-# if user_input.strip().isdecimal() == True:       #checks if it is a decimal type integer
-#     print('input is floating point integer')
-#     exit()
+if user_input.strip().isdecimal() == True:       #checks if it is a decimal type integer
+    print('input is floating point integer')
+    exit()
 
-# if user_input.strip().isalpha() == True:         #checks if given input is alphabetical
-#     print('input is alphabetical')
-#     exit()
+if user_input.strip().isalpha() == True:         #checks if given input is alphabetical
+    print('input is alphabetical')
+    exit()
 
-# if user_input.strip().isalnum() == True:         #checks if input is alphanumerical -- placed here cuz if we place it first, recognizes alphabets and numbers as alphanum 
-#     print('input is alpha-numerical')
-#     exit()
+if user_input.strip().isalnum() == True:         #checks if input is alphanumerical -- placed here cuz if we place it first, recognizes alphabets and numbers as alphanum 
+    print('input is alpha-numerical')
+    exit()
 
 
 
